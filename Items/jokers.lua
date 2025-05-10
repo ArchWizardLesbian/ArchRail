93cb31e5ab34b7207449ea80f113a384d64c035b
ArchWizardLesbian <gostman101@gmail.com>
2025-05-09 22:17:38 -0400

    added a test joker

---
+++ Items/jokers.lua
@@ -0,0 +1,13 @@
+SMODS.Joker {
+    key = "ultraJonkerTest",
+    atlas = 'joke',
+    pos = { x = 0, y = 0 },
+    rarity = 3,
+    cost = 5,
+    unlocked = true,
+    discovered = true,
+    eternal_compat = true,
+    perishable_compat = false,
+    blueprint_compat = true,
+    config = {},
+}
